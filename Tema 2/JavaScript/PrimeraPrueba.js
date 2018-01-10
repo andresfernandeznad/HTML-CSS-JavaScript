713943a92a2bae21e5e1d2b69316a6055688206e
andresfernandeznad <andresfernandeznadales@gmail.com>
2018-01-10 10:36:54 +0100

    Añadidos apuntes de CSS-HTML-Javascript

---
+++ Tema 2/JavaScript/PrimeraPrueba.js
@@ -0,0 +1,5 @@
+//Muestra una alerta que dice hola mundo
+var holaMundo = "Hola Mundo";
+alert(holaMundo);
+var mensaje = "Qué fácil es incluir \'comillas simples\'\n y \"comillas dobles\"";
+alert(mensaje);
